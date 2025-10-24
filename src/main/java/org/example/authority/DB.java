1e713a4eef8c21cbf332b6d88a6415e1fd7695c7
frederik.weitz <frederik.weitz@wpstraining.de>
2025-10-24 09:15:32 +0200

    Datenbanksimulation

---
+++ src/main/java/org/example/authority/DB.java
@@ -0,0 +1,12 @@
+package org.example.authority;
+
+public class DB {
+
+  public static int[][] queryDBForAuthorization() {
+    int[][] arr = new int[3][];
+    arr[0] = new int[]{223, 312, 154, 47, 124,236,334};
+    arr[1] = new int[]{103,401,14,236,56};
+    arr[2] = new int[]{20,312,235,17,124,32};
+    return arr;
+  }
+}
