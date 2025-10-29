8d7c84230ba33b4a041539c594bdcaf25b69f5e1
frederik.weitz <frederik.weitz@wpstraining.de>
2025-10-29 12:25:27 +0100

    Einführung Math-Bibliothek

---
+++ src/main/java/org/example/math/MathUtil.java
@@ -0,0 +1,19 @@
+package org.example.math;
+
+public class MathUtil {
+
+  public static int clampInt(int x, int min, int max) {
+    // Eingabe: zu überprüfender Wert, untere und obere Begrenzung
+    // Ausgabe: Wert im Bereich der Begrenzung
+    // Sonderfälle:
+    //      x ist kleiner als min --> x wird auf min gesetzt
+    //      x ist größer als max  --> x wird auf ma gesetzt
+    //      alle Werte sind gleich
+    //      min und max sind vertauscht
+    //            1. x liegt doch im Bereich (irgendwir), 2. x liegt außerhalb des Bereichs
+
+
+    return 0;
+  }
+
+}
